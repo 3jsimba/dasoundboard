66deef34f3f46ca18c5ddc7a5b4590c682df1aa7
Emmanuel Jerez <ejerez032776@gmail.com>
2022-04-22 03:12:29 -0700

    ver 1.0.4 | vruh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './fonts/publicSans/PublicSans-BlackItalic.ttf';
+import '../public/fonts/publicSans/PublicSans-BlackItalic.ttf';
 import './custom.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
